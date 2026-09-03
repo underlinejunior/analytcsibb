9bad675c0f73f085f1b0abf7c18708b921f0b471
Evaldo Júnior <41309801+underlinejunior@users.noreply.github.com>
2026-09-03 15:33:20 -0300

    Remove trailing slash from Firebase database URL

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG_APP = {
-  FIREBASE_DATABASE_URL: "https://analyticsibb-48aa2-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  FIREBASE_DATABASE_URL: "https://analyticsibb-48aa2-default-rtdb.asia-southeast1.firebasedatabase.app",
   FIREBASE_ROOT: "youtubeDashboard",
   TEMPO_LIMITE_MS: 8000
 };
