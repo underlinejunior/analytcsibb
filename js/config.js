0d381b645e6a6a0e7cac9c79d9728bff1fe44754
Evaldo Júnior <41309801+underlinejunior@users.noreply.github.com>
2026-09-03 11:04:04 -0300

    Update APPS_SCRIPT_URL in config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Cole aqui a URL /exec gerada ao implantar o Google Apps Script como Web App.
 // Exemplo: https://script.google.com/macros/s/AKfycb.../exec
 const CONFIG_APP = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzxBfhfXnm1gayiRYFmSX0WR5jUPQohBkQpnssl19wPPWQkVF8_BhwfNBY8mNZ-p7yzIA/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwye5ADVnV01AbNrrquYvce_06OvyEC7zjPpcwupfJ8QT7JolKKIPJKBV3yL9P04AI91g/exec",
   TEMPO_LIMITE_MS: 30000
 };
