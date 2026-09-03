96e42b6d3876dea3dc8a7eb4f7c2d8d4b7a4a044
Evaldo Júnior <41309801+underlinejunior@users.noreply.github.com>
2026-09-03 15:28:51 -0300

    Change Firebase Database URL in config.js
    Updated Firebase Realtime Database URL in config.

--- js/config.js
+++ js/config.js
@@ -1,9 +1,5 @@
-// Cole aqui a URL EXATA do seu Firebase Realtime Database.
-// Exemplos:
-// https://meu-projeto-default-rtdb.firebaseio.com
-// https://meu-projeto-default-rtdb.us-central1.firebasedatabase.app
 const CONFIG_APP = {
-  FIREBASE_DATABASE_URL: "COLE_AQUI_A_URL_DO_REALTIME_DATABASE",
+  FIREBASE_DATABASE_URL: "https://analyticsibb-48aa2-default-rtdb.asia-southeast1.firebasedatabase.app/",
   FIREBASE_ROOT: "youtubeDashboard",
   TEMPO_LIMITE_MS: 8000
 };
