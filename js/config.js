bfba4e9b3ee4f683d09c06b2d2fe2b124c04e004
Evaldo Júnior <41309801+underlinejunior@users.noreply.github.com>
2026-09-03 12:07:57 -0300

    Clean up comments in config.js
    Removed example comments for Google Apps Script URL.

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
-// Cole aqui a URL /exec gerada ao implantar o Google Apps Script como Web App.
-// Exemplo: https://script.google.com/macros/s/AKfycb.../exec
 const CONFIG_APP = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwye5ADVnV01AbNrrquYvce_06OvyEC7zjPpcwupfJ8QT7JolKKIPJKBV3yL9P04AI91g/exec",
   TEMPO_LIMITE_MS: 30000
