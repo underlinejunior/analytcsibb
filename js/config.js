6219adf0a1426a296abba4a7018a66153f8eb256
Evaldo Júnior <41309801+underlinejunior@users.noreply.github.com>
2026-09-15 07:36:12 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 // Cole aqui a URL /exec gerada ao implantar o Google Apps Script como Web App.
 // Exemplo: https://script.google.com/macros/s/AKfycb.../exec
 const CONFIG_APP = {
+  FIREBASE_DATABASE_URL: "https://analyticsibb-48aa2-default-rtdb.asia-southeast1.firebasedatabase.app",
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwye5ADVnV01AbNrrquYvce_06OvyEC7zjPpcwupfJ8QT7JolKKIPJKBV3yL9P04AI91g/exec",
   TEMPO_LIMITE_MS: 30000
 };
